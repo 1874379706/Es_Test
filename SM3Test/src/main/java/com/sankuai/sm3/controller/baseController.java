29a5915f443bad84b5794d91178d3f80f7d128d5
qiaodexin <1874379706@qq.com>
2025-08-02 11:29:50 +0800

    测试commit

---
+++ SM3Test/src/main/java/com/sankuai/sm3/controller/baseController.java
@@ -0,0 +1,19 @@
+package com.sankuai.sm3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author WXY
+ * @Description TODO
+ * @Date 2025-08-02 11:24
+ * @Version 1.0
+ */
+@Controller
+public class baseController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
